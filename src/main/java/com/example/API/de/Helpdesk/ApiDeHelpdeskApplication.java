3a34699342997790d5a715b8159512d5b83d8f9a
Pilirreos64 <ericdaniel.barreto@gmail.com>
2026-07-29 17:04:23 -0500

    empezamos

---
+++ src/main/java/com/example/API/de/Helpdesk/ApiDeHelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.example.API.de.Helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeHelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeHelpdeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/API/de/Helpdesk/ApiDeHelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API.de.Helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeHelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
