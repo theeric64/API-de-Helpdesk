5a93230e33a5934519a5ac8582b5c017c0d52908
Pilirreos64 <ericdaniel.barreto@gmail.com>
2026-08-07 16:22:05 -0500

    api

---
+++ src/com.helpdesk.api/HelpdeskApiApplication.java
@@ -0,0 +1,14 @@
+
+package com.helpdesk.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpdeskApiApplication.class, args);
+    }
+
+}
